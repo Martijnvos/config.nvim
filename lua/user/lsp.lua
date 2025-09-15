a8dd0ae7e83548e0878923f2ddecd1281a72b460
Martijn Vos <m.a.vos1997@gmail.com>
2025-09-15 23:55:54 +0200

    Support inlay hints and CodeLens in LSP

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -16,9 +16,21 @@ vim.lsp.config('*', {
 -- This is coming from a custom mason registry because of https://github.com/mason-org/mason-registry/pull/6330
 -- source: https://github.com/Crashdummyy/roslynLanguageServer?tab=readme-ov-file
 -- which monitors https://dev.azure.com/azure-public/vside/_artifacts/feed/vs-impl/NuGet/, as described in https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#roslyn_ls
-vim.lsp.config("roslyn", {})
+vim.lsp.config("roslyn", {
+    settings = {
+        ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+        }
+    }
+})
+
+vim.api.nvim_create_augroup("LSP", { clear = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
+    group = "LSP",
+    desc = "Register keybindings",
     callback = function(args)
         local bufopts = { buffer = args.buf }
 
@@ -29,6 +41,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
         vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, bufopts)
 
+        if vim.lsp.inlay_hint then
+            vim.keymap.set("n", "<leader>ih", function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf }), { bufnr = args.buf })
+            end, bufopts)
+        end
+
         -- Dialogs
         vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
         vim.keymap.set("n", "<c-s>", vim.lsp.buf.signature_help, bufopts)
@@ -36,9 +54,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Options
         vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, bufopts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-  end
+    end
 })
 
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
+    group = "LSP",
+    desc = "Auto-trigger CodeLens",
+    callback = function()
+        if vim.lsp.codelens then
+            vim.lsp.codelens.refresh({ bufnr = 0 })
+        end
+    end,
+})
 
 -- Vim Diagnostics
 vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { buffer = 0 })
